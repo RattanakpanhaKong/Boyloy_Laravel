89b0ac221ca93d7841a13870dceffa95213e1d5a
RattanakpanhaKONG <krattanakpanha@gmail.com>
2023-11-01 10:12:02 +0700

    Configure AppServiceProvider to use with WampServer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Schema::defaultStringLength(191);
 
         Gate::define('admin', function ($user) {
             return $user->role == 'admin';
